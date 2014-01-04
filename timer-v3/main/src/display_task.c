db45133218626773460cf7a5d52ee22fd314a10f
Pat S <patrick@satyshur.net>
2014-01-03 22:59:49 -0600

    Added some helper functions and updated the main functions to use the new OLED write method.

--- timer-v3/main/src/display_task.c
+++ timer-v3/main/src/display_task.c
@@ -10,6 +10,7 @@
 
 //Private functions
 void DrawIdleScreen(void);
+void UpdateTimeAndDate(void);
 void UpdateOutputs(void);
 void DrawMenuScreen(void);
 
@@ -141,80 +142,230 @@ void DisplayTaskInit(void)
 void DrawIdleScreen(void)
 {
 	char TimeString[11];
+	MF_StringOptions StringOptions;
+	MF_LineOptions LineOptions;
+
+	LineOptions.LinePattern = 0xFF;
+	LineOptions.LineWeight = 1;
+	LineOptions.LineOptions = 0;
+
+	StringOptions.CharSize = MF_ASCII_SIZE_7X8;
+	StringOptions.StartPadding = 0;
+	StringOptions.EndPadding = 0;
+	StringOptions.TopPadding = 0;
+	StringOptions.BottomPadding = 0;
+	StringOptions.CharacterSpacing = 0;
+	StringOptions.Brightness = 0x0F;
+	StringOptions.FontOptions = OLED_FONT_NORMAL;
 
 	OLED_ClearDisplay();
 
-	//Set up idle screen
+	//Draw the lines around the outputs
+	LineOptions.XStart = 16*4;
+	LineOptions.XEnd = 16*4;
+	LineOptions.YStart = 0;
+	LineOptions.YEnd = 16;
+	OLED_WriteLine2(&LineOptions);
+
+	LineOptions.XStart = 32*4;
+	LineOptions.XEnd = 32*4;
+	LineOptions.YStart = 0;
+	LineOptions.YEnd = 16;
+	OLED_WriteLine2(&LineOptions);
+
+	LineOptions.XStart = 48*4;
+	LineOptions.XEnd = 48*4;
+	LineOptions.YStart = 0;
+	LineOptions.YEnd = 16;
+	OLED_WriteLine2(&LineOptions);
+
+	LineOptions.XStart = 0;
+	LineOptions.XEnd = 255;
+	LineOptions.YStart = 16;
+	LineOptions.YEnd = 16;
+	OLED_WriteLine2(&LineOptions);
+
+	//Display the labels for time, date and mode
+	//StringOptions.CharSize = MF_ASCII_SIZE_7X8;
+	StringOptions.XStart = (IDLE_TIME_COLUMN-2)*4;
+	StringOptions.YStart = IDLE_TIME_ROW+18;
+	OLED_WriteMFString2("Time:", &StringOptions);
+
+	StringOptions.XStart = (IDLE_DATE_COLUMN-2)*4;
+	StringOptions.YStart = IDLE_DATE_ROW+18;
+	OLED_WriteMFString2("Date:", &StringOptions);
+
+	StringOptions.XStart = (IDLE_STATUS_COLUMN-11)*4;
+	StringOptions.YStart = IDLE_STATUS_ROW;
+	OLED_WriteMFString2("Mode:", &StringOptions);
+
+	//TODO: move this one to the update output function
+	StringOptions.XStart = (IDLE_STATUS_COLUMN)*4;
+	StringOptions.YStart = IDLE_STATUS_ROW;
+	OLED_WriteMFString2("Auto  ", &StringOptions);
+
+	//Display the current time
 	DS3232M_GetTimeString(TimeString, 0);
-	OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, TimeString, IDLE_TIME_COLUMN, IDLE_TIME_ROW, OLED_FONT_NORMAL);
+	StringOptions.CharSize = MF_ASCII_SIZE_WA;
+	StringOptions.XStart = (IDLE_TIME_COLUMN)*4;
+	StringOptions.YStart = IDLE_TIME_ROW;
+	OLED_WriteMFString2(TimeString, &StringOptions);
 
-	OLED_WriteLine(16*4, 0, 16*4, 16, 1, 1);
-	OLED_WriteLine(32*4, 0, 32*4, 16, 1, 1);
-	OLED_WriteLine(48*4, 0, 48*4, 16, 1, 1);
-	OLED_WriteLine(0, 16, 255, 16, 1, 1);
-
-	OLED_WriteMFString(MF_ASCII_SIZE_7X8, "Time:", IDLE_TIME_COLUMN-2, IDLE_TIME_ROW+18, OLED_FONT_NORMAL);
-	OLED_WriteMFString(MF_ASCII_SIZE_7X8, "Date:", IDLE_DATE_COLUMN-2, IDLE_DATE_ROW+18, OLED_FONT_NORMAL);
-	OLED_WriteMFString(MF_ASCII_SIZE_7X8, "Mode:", IDLE_STATUS_COLUMN-11, IDLE_STATUS_ROW, OLED_FONT_NORMAL);
-	OLED_WriteMFString(MF_ASCII_SIZE_7X8, "Auto  ", IDLE_STATUS_COLUMN, IDLE_STATUS_ROW, OLED_FONT_NORMAL);
-
+	//Display the current date
+	//StringOptions.CharSize = MF_ASCII_SIZE_WA;
 	DS3232M_GetDateString(TimeString, 0);
-	OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, TimeString, IDLE_DATE_COLUMN, IDLE_DATE_ROW, OLED_FONT_NORMAL);
+	StringOptions.XStart = (IDLE_DATE_COLUMN)*4;
+	StringOptions.YStart = IDLE_DATE_ROW;
+	OLED_WriteMFString2(TimeString, &StringOptions);
 
 	UpdateOutputs();
 
 	return;
 }
 
+void UpdateTimeAndDate(void)
+{
+	char TimeString[11];
+	MF_StringOptions StringOptions;
+
+	StringOptions.CharSize = MF_ASCII_SIZE_7X8;
+	StringOptions.StartPadding = 0;
+	StringOptions.EndPadding = 20;
+	StringOptions.TopPadding = 0;
+	StringOptions.BottomPadding = 0;
+	StringOptions.CharacterSpacing = 0;
+	StringOptions.Brightness = 0x0F;
+	StringOptions.FontOptions = OLED_FONT_NORMAL;
+
+	//Display the current time
+	DS3232M_GetTimeString(TimeString, 0);
+	StringOptions.CharSize = MF_ASCII_SIZE_WA;
+	StringOptions.XStart = (IDLE_TIME_COLUMN)*4;
+	StringOptions.YStart = IDLE_TIME_ROW;
+	OLED_WriteMFString2(TimeString, &StringOptions);
+
+	//Display the current date
+	DS3232M_GetDateString(TimeString, 0);
+	StringOptions.XStart = (IDLE_DATE_COLUMN)*4;
+	StringOptions.YStart = IDLE_DATE_ROW;
+	OLED_WriteMFString2(TimeString, &StringOptions);
+
+	return;
+}
+
 void DrawMenuScreen(void)
 {
+	MF_StringOptions StringOptions;
+	MF_LineOptions LineOptions;
+
+	LineOptions.LinePattern = 0xFF;
+	LineOptions.LineWeight = 1;
+	LineOptions.LineOptions = 0;
+
+	StringOptions.CharSize = MF_ASCII_SIZE_7X8;
+	StringOptions.StartPadding = 0;
+	StringOptions.EndPadding = 0;
+	StringOptions.TopPadding = 0;
+	StringOptions.BottomPadding = 0;
+	StringOptions.CharacterSpacing = 0;
+	StringOptions.Brightness = 0x0F;
+	StringOptions.FontOptions = OLED_FONT_NORMAL;
+
 	OLED_ClearDisplay();
-	OLED_WriteMFString(MF_ASCII_SIZE_7X8, "Menu", 2, 55, OLED_FONT_NORMAL);
-	OLED_WriteLine(0, 53, 255, 53, 1, 1);
+
+	StringOptions.XStart = 8;
+	StringOptions.YStart = 55;
+	OLED_WriteMFString2("Menu", &StringOptions);
+
+	LineOptions.XStart = 0;
+	LineOptions.XEnd = 255;
+	LineOptions.YStart = 53;
+	LineOptions.YEnd = 53;
+	OLED_WriteLine2(&LineOptions);
+
 	return;
 }
 
 void UpdateOutputs(void)
 {
 	uint8_t OutputState;
+	MF_StringOptions StringOptions;
+
+	StringOptions.CharSize = MF_ASCII_SIZE_WA;
+	StringOptions.StartPadding = 0;
+	StringOptions.EndPadding = 0;
+	StringOptions.TopPadding = 0;
+	StringOptions.BottomPadding = 0;
+	StringOptions.CharacterSpacing = 0;
+	StringOptions.Brightness = 0x0F;
+
+
+
+
+
+
 
 	OutputState = TimerGetOutputState();
 
+	//Output 1
+	StringOptions.XStart = 12;
+	StringOptions.YStart = 0;
 	if((OutputState & 0x01) == 0x01)
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 1", 3, 0, OLED_FONT_INVERSE);
+		StringOptions.FontOptions = OLED_FONT_INVERSE;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 1", 3, 0, OLED_FONT_INVERSE);
 	}
 	else
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 1", 3, 0, OLED_FONT_NORMAL);
+		StringOptions.FontOptions = OLED_FONT_NORMAL;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 1", 3, 0, OLED_FONT_NORMAL);
 	}
+	OLED_WriteMFString2("Out 1", &StringOptions);
 
+	//Output 2
+	StringOptions.XStart = 76;
+	StringOptions.YStart = 0;
 	if((OutputState & 0x02) == 0x02)
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 2", 19, 0, OLED_FONT_INVERSE);
+		StringOptions.FontOptions = OLED_FONT_INVERSE;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 2", 19, 0, OLED_FONT_INVERSE);
 	}
 	else
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 2", 19, 0, OLED_FONT_NORMAL);
+		StringOptions.FontOptions = OLED_FONT_NORMAL;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 2", 19, 0, OLED_FONT_NORMAL);
 	}
+	OLED_WriteMFString2("Out 2", &StringOptions);
 
+	//Output 3
+	StringOptions.XStart = 140;
+	StringOptions.YStart = 0;
 	if((OutputState & 0x04) == 0x04)
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 3", 35, 0, OLED_FONT_INVERSE);
+		StringOptions.FontOptions = OLED_FONT_INVERSE;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 3", 35, 0, OLED_FONT_INVERSE);
 	}
 	else
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 3", 35, 0, OLED_FONT_NORMAL);
+		StringOptions.FontOptions = OLED_FONT_NORMAL;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 3", 35, 0, OLED_FONT_NORMAL);
 	}
+	OLED_WriteMFString2("Out 3", &StringOptions);
 
+	//Output 4
+	StringOptions.XStart = 204;
+	StringOptions.YStart = 0;
 	if((OutputState & 0x08) == 0x08)
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 4", 51, 0, OLED_FONT_INVERSE);
+		StringOptions.FontOptions = OLED_FONT_INVERSE;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 4", 51, 0, OLED_FONT_INVERSE);
 	}
 	else
 	{
-		OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 4", 51, 0, OLED_FONT_NORMAL);
+		StringOptions.FontOptions = OLED_FONT_NORMAL;
+		//OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, "Out 4", 51, 0, OLED_FONT_NORMAL);
 	}
+	OLED_WriteMFString2("Out 4", &StringOptions);
 
 	return;
 }
@@ -224,11 +375,13 @@ void DisplayTask(void *pvParameters)
 {
 	OLED_Command CommandToExecute;
 
+	//MF_StringOptions StringOptions;
+
 	uint8_t CurrentMenuItem = 0;
 	uint8_t PreviousMenuItem = 1;	//Note: this cannot be zero, or the menu screen will not be drawn after the first button press
 	//TimeAndDate CurrentTime;
 
-	char TimeString[11];
+	//char TimeString[11];
 
 	uint8_t DisplayStatus = DISPLAY_STATUS_IDLE_BRIGHT;
 	portBASE_TYPE QueueStatus;
@@ -242,7 +395,7 @@ void DisplayTask(void *pvParameters)
 	if(App_GetStatus() == APP_STATUS_OK)
 	{
 		OLED_WriteMFString(MF_ASCII_SIZE_7X8, InitString, 16, 28, OLED_FONT_NORMAL);
-		OLED_WriteMFString(MF_ASCII_SIZE_7X8, InitString, 1, 1, OLED_FONT_INVERSE);
+		//OLED_WriteMFString(MF_ASCII_SIZE_7X8, InitString, 1, 1, OLED_FONT_INVERSE);
 		vTaskDelay(3000);
 		OLED_ClearDisplay();
 	}
@@ -364,12 +517,8 @@ void DisplayTask(void *pvParameters)
 				case OLED_CMD_TIME_IN:
 					if( (DisplayStatus != DISPLAY_STATUS_MENU) )
 					{
-						//TODO: Clear the extra stuff from the display. WHen going from longer strings to shorter strings (12:59 -> 1:00) there is extra stuff on the display.
-						OLED_ClearWindow(IDLE_TIME_COLUMN, IDLE_DATE_COLUMN+24, IDLE_TIME_ROW, IDLE_TIME_ROW+16);	//TODO: This sort of causes flickering, maybe fix it later
-						DS3232M_GetTimeString(TimeString, 0);
-						OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, TimeString, IDLE_TIME_COLUMN, IDLE_TIME_ROW, OLED_FONT_NORMAL);
-						DS3232M_GetDateString(TimeString, 0);
-						OLED_WriteMFString_WA(MF_ASCII_SIZE_WA, TimeString, IDLE_DATE_COLUMN, IDLE_DATE_ROW, OLED_FONT_NORMAL);
+						//TODO: Clear the extra stuff from the display. WHen going from longer strings to shorter strings (12:59 -> 1:00) there is extra stuff on the display. (should be fixed, check this...)
+						UpdateTimeAndDate();
 						UpdateOutputs();
 					}
 					break;

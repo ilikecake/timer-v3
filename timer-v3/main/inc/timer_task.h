4670c7a4822aeb659f72ed8658724146651ad93d
Pat S <patrick@satyshur.net>
2014-01-05 12:53:37 -0600

    Added functions to save and load tasks from EEPROM.

--- timer-v3/main/inc/timer_task.h
+++ timer-v3/main/inc/timer_task.h
@@ -11,14 +11,24 @@
 #define TIMER_TASK_CMD_START	0
 #define TIMER_TASK_CMD_STOP		1
 #define TIMER_TASK_CMD_TICK		2
+#define TIMER_TASK_CMD_PAUSE	3
 
+#define TIMER_STATUS_OFF		0x00
+#define TIMER_STATUS_ON			0x01
+#define TIMER_STATUS_PAUSED		0x02	//TODO: Pause the timer task for a certain length of time. We will probably want to set the override status here...
 
+#define TIMER_EEPROM_START_ADDRESS			0x00		/** The address to store the first timer events in EEPROM. Subsequent events will be placed sequentially after this event. */
 
+#define TIMER_OUTPUT_NUMBER					4			/** The number of outputs, starting at one. Setting this to 4 means that there are four outputs*/
+#define TIMER_EVENT_NUMBER					6			/** The number of events for each timer, starting at one. Setting this to 6 means there are 6 events*/
 
 #define TIMER_TASK_TYPE_TIME_EVENT			0x01		//Turn the output on or off at a certain time
 #define TIMER_TASK_TYPE_REPEATING_EVENT		0x02		//Turn the output on or off on a certain interval (for this type, only one event can be specified)
 #define TIMER_TASK_TYPE_SUN_EVENT			0x03		//Turn the output on or off based on sunrise/sunset time
 #define TIMER_TASK_TYPE_STEADY_EVENT		0x04		//The output is always on or off (for this type, only one event can be specified)
+
+//A FreeRTOS queue to receive commands for the timer task
+xQueueHandle xTimerCommands;
 
 
 /* EventType:
@@ -40,15 +50,31 @@ typedef struct __attribute__((packed)){
 } TimerEvent;
 
 uint8_t TimerGetOutputState(void);
+uint8_t TimerGetTimerState(void);
+
+//Write all events from the global event list to EEPROM
+uint8_t TimerWriteEventsToEEPROM(void);
+
+//Read all events from EEPROM and write them to the global event list
+uint8_t TimerReadEventsFromEEPROM(void);
+
+//Read a single event from EEPROM into a temporary EventData struct
+uint8_t TimerReadSingleEventFromEEPROM(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent *EventData);
+
+//Write a single event from an EventData struct into a single EEPROM event slot
+uint8_t TimerWriteSingleEventToEEPROM(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent *EventData);
 
 void TimerGetEvent(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent *EventData);
 void TimerSetEvent(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent *EventData);
+void TimerClearRamEvent(uint8_t OutputNumber, uint8_t EventNumber);
 
 uint8_t InitTimerTask(void);
 void TimerTask(void *pvParameter);
 
 
 void TimerSetOutput(uint8_t OutputNumber, uint8_t OutputState);
+
+void TimerUpdateOutputs(void);
 
 
 
--- timer-v3/main/src/timer_task.c
+++ timer-v3/main/src/timer_task.c
@@ -7,54 +7,179 @@
 
 #include "main.h"
 
-//Six possible events are defined for each output...
-//TODO: make this a compiler directive for easy modificaiton
-TimerEvent TimerEventList[4][6];
+//Four outputs, six events per output
+TimerEvent TimerEventList[TIMER_OUTPUT_NUMBER][TIMER_EVENT_NUMBER];
 
 //TODO: make some indication of the most recent event for each output. This can be used to determine if an event was skipped...
 uint8_t OutputStatus;
+uint8_t TimerStatus;
 
 uint8_t TimerGetOutputState(void)
 {
 	return OutputStatus;
 }
 
-/*
+uint8_t TimerGetTimerState(void)
+{
+	return TimerStatus;
+}
+
+/*Get a timer event from RAM
  *
- * OutputNumber: The desired output (1-4)
- * EventNumber: The desired event (1-6)
+ * OutputNumber: The desired output, starts at zero
+ * EventNumber: The desired event, Starts at zero
  * EventData: The event data copied from the event list
  */
 void TimerGetEvent(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent *EventData)
 {
-	if( (OutputNumber > 0) && (OutputNumber < 5) && (EventNumber > 0) && (EventNumber < 7) )
+	if( (OutputNumber >= 0) && (OutputNumber < TIMER_OUTPUT_NUMBER) && (EventNumber >= 0) && (EventNumber < TIMER_EVENT_NUMBER) )
 	{
-		EventData->EventType		= TimerEventList[OutputNumber-1][EventNumber-1].EventType;
-		EventData->EventTime[0]		= TimerEventList[OutputNumber-1][EventNumber-1].EventTime[0];
-		EventData->EventTime[1]		= TimerEventList[OutputNumber-1][EventNumber-1].EventTime[1];
-		EventData->EventTime[2]		= TimerEventList[OutputNumber-1][EventNumber-1].EventTime[2];
-		EventData->EventOutputState	= TimerEventList[OutputNumber-1][EventNumber-1].EventOutputState;
+		EventData->EventType		= TimerEventList[OutputNumber][EventNumber].EventType;
+		EventData->EventTime[0]		= TimerEventList[OutputNumber][EventNumber].EventTime[0];
+		EventData->EventTime[1]		= TimerEventList[OutputNumber][EventNumber].EventTime[1];
+		EventData->EventTime[2]		= TimerEventList[OutputNumber][EventNumber].EventTime[2];
+		EventData->EventOutputState	= TimerEventList[OutputNumber][EventNumber].EventOutputState;
 	}
 	return;
 }
 
-/*
+/*Set a timer event in RAM
  *
- * OutputNumber: The desired output (1-4)
- * EventNumber: The desired event (1-6)
+ * OutputNumber: The desired output, starts at zero
+ * EventNumber: The desired event, starts at zero
  * EventData: The event data copied from the event list
  */
 void TimerSetEvent(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent *EventData)
 {
-	if( (OutputNumber > 0) && (OutputNumber < 5) && (EventNumber > 0) && (EventNumber < 7) )
+	if( (OutputNumber >= 0) && (OutputNumber < TIMER_OUTPUT_NUMBER) && (EventNumber >= 0) && (EventNumber < TIMER_EVENT_NUMBER) )
 	{
-		TimerEventList[OutputNumber-1][EventNumber-1].EventType = EventData->EventType;
-		TimerEventList[OutputNumber-1][EventNumber-1].EventTime[0] = EventData->EventTime[0];
-		TimerEventList[OutputNumber-1][EventNumber-1].EventTime[1] = EventData->EventTime[1];
-		TimerEventList[OutputNumber-1][EventNumber-1].EventTime[2] = EventData->EventTime[2];
-		TimerEventList[OutputNumber-1][EventNumber-1].EventOutputState = EventData->EventOutputState;
+		TimerEventList[OutputNumber][EventNumber].EventType = EventData->EventType;
+		TimerEventList[OutputNumber][EventNumber].EventTime[0] = EventData->EventTime[0];
+		TimerEventList[OutputNumber][EventNumber].EventTime[1] = EventData->EventTime[1];
+		TimerEventList[OutputNumber][EventNumber].EventTime[2] = EventData->EventTime[2];
+		TimerEventList[OutputNumber][EventNumber].EventOutputState = EventData->EventOutputState;
 	}
 	return;
+}
+
+/*Clear a timer event in RAM
+ * All timer event information is set to zero.
+ *
+ * OutputNumber: The desired output, starts at zero
+ * EventNumber: The desired event, starts at zero
+ */
+void TimerClearRamEvent(uint8_t OutputNumber, uint8_t EventNumber)
+{
+	if( (OutputNumber >= 0) && (OutputNumber < TIMER_OUTPUT_NUMBER) && (EventNumber >= 0) && (EventNumber < TIMER_EVENT_NUMBER) )
+	{
+		TimerEventList[OutputNumber][EventNumber].EventType			= 0;
+		TimerEventList[OutputNumber][EventNumber].EventTime[0]		= 0;
+		TimerEventList[OutputNumber][EventNumber].EventTime[1]		= 0;
+		TimerEventList[OutputNumber][EventNumber].EventTime[2]		= 0;
+		TimerEventList[OutputNumber][EventNumber].EventOutputState	= 0;
+	}
+	return;
+}
+
+/**Write all events from the global event list to EEPROM
+ *  TODO: make this return the maximum EEPROM address used?
+ *
+ *  Returns 0xFF if there is an error reading, otherwise returns 0
+ */
+uint8_t TimerWriteEventsToEEPROM(void)
+{
+	uint8_t i, j, sz;
+	uint16_t addr;
+
+	addr = TIMER_EEPROM_START_ADDRESS;		//The address of the current struct in EEPROM
+	sz = sizeof(TimerEventList[0][0]);		//The size of the structs, this should always be the same
+
+	for(i=0; i<TIMER_OUTPUT_NUMBER; i++)
+	{
+		for(j=0; j<TIMER_EVENT_NUMBER; j++)
+		{
+			if( EEPROM_Write(addr, &TimerEventList[i][j], sz ) != 0)
+			{
+				return 0xFF;
+			}
+			addr += sz;
+		}
+	}
+
+	return 0x00;
+}
+
+/**Read all events from EEPROM and write them to the global event list
+ *
+ *  Returns 0xFF if there is an error reading, otherwise returns 0
+ */
+uint8_t TimerReadEventsFromEEPROM(void)
+{
+	uint8_t i, j, sz;
+	uint16_t addr;
+
+	addr = TIMER_EEPROM_START_ADDRESS;		//The address of the current struct in EEPROM
+	sz = sizeof(TimerEventList[0][0]);		//The size of the structs, this should always be the same
+
+	for(i=0; i<TIMER_OUTPUT_NUMBER; i++)
+	{
+		for(j=0; j<TIMER_EVENT_NUMBER; j++)
+		{
+			if( EEPROM_Read(addr, &TimerEventList[i][j], sz ) != 0)
+			{
+				return 0xFF;
+			}
+			addr += sz;
+		}
+	}
+
+	return 0x00;
+}
+
+/**Read a single event from EEPROM into a temporary EventData struct
+ * 	OutputNumber: Starts at zero, indicates the output number of the event to get.
+ * 	EventNumber: Starts at zero, indicates the event number to get.
+ * 	EventData: A pointer to the TimerEvent struct to put the data
+ *
+ *  Returns 0xFF if there is an error reading, otherwise returns 0
+ */
+uint8_t TimerReadSingleEventFromEEPROM(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent* EventData)
+{
+	uint8_t sz;
+	uint16_t addr;
+
+	sz = sizeof(*EventData);																	//The size of the TimerEvent struct
+	addr = TIMER_EEPROM_START_ADDRESS + sz*(OutputNumber*TIMER_EVENT_NUMBER + EventNumber);		//The address of the current struct in EEPROM
+
+	if( EEPROM_Read(addr, EventData, sz ) != 0)
+	{
+		return 0xFF;
+	}
+
+	return 0x00;
+}
+
+/**Write a single event from an EventData struct into a single EEPROM event slot
+ * 	OutputNumber: Starts at zero, indicates the output number of the event to set.
+ * 	EventNumber: Starts at zero, indicates the event number to set.
+ * 	EventData: A pointer to the TimerEvent struct to copy to EEPROM.
+ *
+ *  Returns 0xFF if there is an error reading, otherwise returns 0
+ */
+uint8_t TimerWriteSingleEventToEEPROM(uint8_t OutputNumber, uint8_t EventNumber, TimerEvent *EventData)
+{
+	uint8_t sz;
+	uint16_t addr;
+
+	sz = sizeof(*EventData);																	//The size of the TimerEvent struct
+	addr = TIMER_EEPROM_START_ADDRESS + sz*(OutputNumber*TIMER_EVENT_NUMBER + EventNumber);		//The address of the current struct in EEPROM
+
+	if( EEPROM_Write(addr, &EventData, sz ) != 0)
+	{
+		return 0xFF;
+	}
+
+	return 0x00;
 }
 
 uint8_t InitTimerTask(void)
@@ -62,13 +187,20 @@ uint8_t InitTimerTask(void)
 	//TODO: Code to read timer events from EEPROM goes here...
 	//TODO: Check if timer tasks are not set?
 
+	if(TimerReadEventsFromEEPROM() != 0)
+	{
+		return 0x01;	//TODO: Make this a specific error code
+	}
+
+	TimerStatus = TIMER_STATUS_OFF;
+
 	OutputStatus = 0x00;
+	TimerSetOutput(0, 0);
 	TimerSetOutput(1, 0);
 	TimerSetOutput(2, 0);
 	TimerSetOutput(3, 0);
-	TimerSetOutput(4, 0);
 
-	TimerEventList[0][0].EventType = TIMER_TASK_TYPE_STEADY_EVENT;
+	/*TimerEventList[0][0].EventType = TIMER_TASK_TYPE_STEADY_EVENT;
 	TimerEventList[0][0].EventTime[0] = 0;
 	TimerEventList[0][0].EventTime[0] = 0;
 	TimerEventList[0][0].EventTime[0] = 0;
@@ -90,20 +222,18 @@ uint8_t InitTimerTask(void)
 	TimerEventList[3][0].EventTime[0] = 0;
 	TimerEventList[3][0].EventTime[0] = 0;
 	TimerEventList[3][0].EventTime[0] = 0;
-	TimerEventList[3][0].EventOutputState = 0;
+	TimerEventList[3][0].EventOutputState = 0;*/
 
 	xTimerCommands = xQueueCreate( 2, sizeof( uint8_t ) );
 
 	return 0;
 }
 
-
-
 void TimerTask(void *pvParameter)
 {
-	TimeAndDate CurrentTime;
-	uint8_t i;
-	uint8_t j;
+	//TimeAndDate CurrentTime;
+	//uint8_t i;
+	//uint8_t j;
 	uint8_t TimerCommand;
 
 	//Wake the task here on minute interrupt
@@ -113,56 +243,94 @@ void TimerTask(void *pvParameter)
 		//This task should wake before the OLED task
 		xQueueReceive(xTimerCommands, &TimerCommand, portMAX_DELAY);
 
-		if(TimerCommand == TIMER_TASK_CMD_TICK)
+		switch(TimerCommand)
 		{
+			case TIMER_TASK_CMD_TICK:
+				TimerUpdateOutputs();
+				break;
 
-			DS3232M_GetTime(&CurrentTime);
+			case TIMER_TASK_CMD_START:
+				TimerStatus = TIMER_STATUS_ON;
+				TimerUpdateOutputs();
+				break;
 
-			//Change day of week to bitmapped variable
-			CurrentTime.dow = (1<<CurrentTime.dow);
+			case TIMER_TASK_CMD_STOP:
+				TimerStatus = TIMER_STATUS_OFF;
+				TimerSetOutput(0, 0);
+				TimerSetOutput(1, 0);
+				TimerSetOutput(2, 0);
+				TimerSetOutput(3, 0);
+				break;
 
-			for(j=0;j<4;j++)
-			{
-				switch(TimerEventList[j][0].EventType)
-				{
-					case TIMER_TASK_TYPE_REPEATING_EVENT:
-						//printf("Repeating Event\r\n");
-						//stuff goes here...
-						break;
+			case TIMER_TASK_CMD_PAUSE:
+				TimerStatus = TIMER_STATUS_PAUSED;
+				//Add something here to set the outputs to their override state.
+				break;
 
-					case TIMER_TASK_TYPE_STEADY_EVENT:
-						//printf("Steady Event: Output %u state %u\r\n", j, TimerEventList[j][0].EventOutputState);
-
-						if( (OutputStatus & (1<<(j))) != TimerEventList[j][0].EventOutputState)
-						{
-							TimerSetOutput(j, TimerEventList[j][0].EventOutputState);
-						}
-						break;
-
-					case TIMER_TASK_TYPE_TIME_EVENT:
-						//printf("Timed event\r\n");
-						for(i=0; i<6; i++)
-						{
-							if( ((TimerEventList[j][i].EventTime[0] & CurrentTime.dow) == CurrentTime.dow)  && (TimerEventList[j][i].EventTime[1] == CurrentTime.hour) && (TimerEventList[j][i].EventTime[2] == CurrentTime.min) )
-							{
-								//Time matches, set the output to the new state
-								//printf("Match %u output to %u\r\n", i, TimerEventList[j][i].EventOutputState);
-								TimerSetOutput(j, TimerEventList[j][i].EventOutputState);
-							}
-						}
-						break;
-
-					case TIMER_TASK_TYPE_SUN_EVENT:
-						//printf("Sun based event\r\n");
-						//I need to get a sunrise/sunset table or calculator here....
-						break;
-				}
-			}
 
 		}
+
 	}
 
-	//Call the Display update task here
+	return;
+}
+
+
+/** Update all the outputs based on the current time */
+void TimerUpdateOutputs(void)
+{
+	TimeAndDate CurrentTime;
+	uint8_t j;
+	uint8_t i;
+
+	if(TimerStatus == TIMER_STATUS_ON)
+	{
+		DS3232M_GetTime(&CurrentTime);
+
+		//Change day of week to bitmapped variable
+		CurrentTime.dow = (1<<CurrentTime.dow);
+
+		//Count through the outputs
+		for(j=0; j<4; j++)
+		{
+			switch(TimerEventList[j][0].EventType)
+			{
+			case TIMER_TASK_TYPE_REPEATING_EVENT:
+				//printf("Repeating Event\r\n");
+				//stuff goes here...
+				break;
+
+			case TIMER_TASK_TYPE_STEADY_EVENT:
+				//printf("Steady Event: Output %u state %u\r\n", j, TimerEventList[j][0].EventOutputState);
+
+				if( (OutputStatus & (1<<(j))) != TimerEventList[j][0].EventOutputState)
+				{
+					TimerSetOutput(j, TimerEventList[j][0].EventOutputState);
+				}
+				break;
+
+			case TIMER_TASK_TYPE_TIME_EVENT:
+				//printf("Timed event\r\n");
+				for(i=0; i<6; i++)
+				{
+					if( ((TimerEventList[j][i].EventTime[0] & CurrentTime.dow) == CurrentTime.dow)  && (TimerEventList[j][i].EventTime[1] == CurrentTime.hour) && (TimerEventList[j][i].EventTime[2] == CurrentTime.min) )
+					{
+						//Time matches, set the output to the new state
+						//printf("Match %u output to %u\r\n", i, TimerEventList[j][i].EventOutputState);
+						TimerSetOutput(j, TimerEventList[j][i].EventOutputState);
+					}
+				}
+				break;
+
+			case TIMER_TASK_TYPE_SUN_EVENT:
+				//printf("Sun based event\r\n");
+				//I need to get a sunrise/sunset table or calculator here....
+				break;
+			}
+		}
+
+	}
+
 	return;
 }
 
